b98145edc14f76d7754380aa0a061d89b8750ddc
Marcos Krul <marcoskrul2k@gmail.com>
2022-08-25 22:43:19 -0300

    refactor

--- src/index.py
+++ src/index.py
@@ -1,9 +1,7 @@
 import handle_car_dataset
 import handle_congressional_dataset
 import handle_leaf_dataset
-import arvore_decisao
 import naive_bayes
-import plot_bar
 
 if __name__ == '__main__':
 
@@ -19,92 +17,32 @@ if __name__ == '__main__':
     print('Entrada inválida')
     exit()
 
-  resultados = []
-
   if entrada == 1:
     base_treino, base_teste, classificacoes_treino, classificacoes_teste = handle_car_dataset.get()
-    a1 = naive_bayes.exec(
+    naive_bayes.exec(
       base_treino, 
       base_teste, 
       classificacoes_treino, 
       classificacoes_teste, 
       title_base=labels_base[entrada-1]
     )
-    a2 = arvore_decisao.exec(
-      base_treino, 
-      base_teste, 
-      classificacoes_treino, 
-      classificacoes_teste, 
-      class_names=['unacc', 'acc', 'good', 'v-good'],
-      feature_names=['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety']
-    )
-
-    resultados.append(a1)
-    resultados.append(a2)
 
   if entrada == 2:
     base_treino, base_teste, classificacoes_treino, classificacoes_teste = handle_congressional_dataset.get()
-    a1 = naive_bayes.exec(
+    naive_bayes.exec(
       base_treino, 
       base_teste, 
       classificacoes_treino, 
       classificacoes_teste, 
       title_base=labels_base[entrada-1]
     )
-    a2 = arvore_decisao.exec(
-      base_treino, 
-      base_teste, 
-      classificacoes_treino, 
-      classificacoes_teste, 
-      class_names=['democrat', 'republican'],
-      feature_names=[
-        'handicapped-infants', 'water-project-cost-sharing',
-        'adoption-of-the-budget-resolution',
-        'physician-fee-freeze', 'el-salvador-aid',
-        'religious-groups-in-schools',
-        'anti-satellite-test-ban', 'aid-to-nicaraguan-contras',
-        'mx-missile','immigration', 'synfuels-corporation-cutback',
-        'education-spending', 'superfund-right-to-sue','crime',
-        'duty-free-exports', 'export-administration-act-south-africa'
-      ]
-    )
-
-    resultados.append(a1)
-    resultados.append(a2)
 
   if entrada == 3:
-    base_treino, base_teste, classificacoes_treino, classificacoes_teste = handle_leaf_dataset.get()
-    a1 = naive_bayes.exec(
+    base_treino, base_teste, classificacoes_treino, classificacoes_teste = handle_leaf_dataset.get(mapear=False)
+    naive_bayes.exec(
       base_treino, 
       base_teste, 
       classificacoes_treino, 
       classificacoes_teste,
       title_base=labels_base[entrada-1]
     )
-    a2 = arvore_decisao.exec(
-      base_treino, 
-      base_teste, 
-      classificacoes_treino, 
-      classificacoes_teste, 
-      feature_names=[
-        'Specimen Number', 'Eccentricity',
-        'Aspect Ratio', 'Elongation',
-        'Solidity', 'Stochastic Convexity',
-        'Isoperimetric Factor', 'Maximal Indentation Depth',
-        'Lobedness', 'Average Intensity',
-        'Average Contrast', 'Smoothness',
-        'Third moment', 'Uniformity', 'Entropy',
-      ]
-    )
-
-    resultados.append(a1)
-    resultados.append(a2)
-
-  plot_bar.exec(
-    x_label='Algoritmo',
-    y_label='Acurácia (%)',
-    data=resultados,
-    labels=['Naive Bayes', 'Árvore de Decisão'],
-    title=f'Comparação modelos de machine learning base {labels_base[entrada-1]}',
-    porcentagem=True
-  )
--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -1,43 +1,65 @@
+import os
 import warnings
 import plot_bar
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from datetime import datetime
 from prettytable import PrettyTable
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 def exec(base_treino, base_teste, classificacoes_treino, classificacoes_teste, title_base):
   warnings.filterwarnings('ignore')
 
-  naive_bayes = GaussianNB()
-  naive_bayes.fit(base_treino, classificacoes_treino)
-  previsoes = naive_bayes.predict(base_teste)
-  acuracia = accuracy_score(classificacoes_teste, previsoes)
+  models = []
+  resultados = []
+
+  models.append(('Árvore de Decisão', DecisionTreeClassifier(random_state=0)))
+  models.append(('Naive bayes Gaussian', GaussianNB()))
+  models.append(('Naive bayes Multinomial', MultinomialNB()))
+  models.append(('Naive bayes Bernoulli', BernoulliNB()))
+
+  for nome, modelo in models:
+    modelo.fit(base_treino, classificacoes_treino)
+    previsoes = modelo.predict(base_teste)
+    acuracia = accuracy_score(classificacoes_teste, previsoes)
+    resultados.append(acuracia)
+
+    if nome == 'Árvore de Decisão':
+      now = datetime.now()
+      dt_string = now.strftime("%d_%m_%Y-%H_%M_%S")
+      
+      export_graphviz(
+        modelo,
+        class_names=modelo.classes_,
+        filled=True, rounded=True, 
+        out_file=os.path.join(os.path.dirname(__file__), f'../tmp/dot_files/tree_{dt_string}.dot')
+      )
+    
+    tabela = PrettyTable()
+    matriz = confusion_matrix(classificacoes_teste, previsoes)
+    tabela.title = 'Matriz de confusão'
+    tabela.field_names = ['', *modelo.classes_]
+
+    for i in range(0, len(matriz)):
+      row = [modelo.classes_[i]]
+      tabela.add_row([*row, *matriz[i]])
+    
+    print('===================================================================================')
+    print(nome)
+    print(f'Quantidade de propriedades: {modelo.n_features_in_}')
+    print(f'Acurácia do modelo: {(acuracia*100):,.2f}% ({acuracia})')
+    print(classification_report(classificacoes_teste, previsoes))
+    print(tabela)
+    print('===================================================================================')
 
   plot_bar.exec(
-    x_label='Classes',
-    y_label='Quantidade de registros',
-    data=naive_bayes.class_count_,
-    labels=naive_bayes.classes_,
-    title=f'Balanceamento da base de dados de treino {title_base}'
+    x_label='Algoritmo',
+    y_label='Acurácia (%)',
+    data=resultados,
+    labels=[x for x, _ in models],
+    title=f'Comparação modelos de machine learning base {title_base}',
+    porcentagem=True
   )
-
-  tabela = PrettyTable()
-  matriz = confusion_matrix(classificacoes_teste, previsoes)
-  tabela.title = 'Matriz de confusão'
-  tabela.field_names = ['', *naive_bayes.classes_]
-
-  for i in range(0, len(matriz)):
-    row = [naive_bayes.classes_[i]]
-    tabela.add_row([*row, *matriz[i]])
-
-  print('===================================================================================')
-  print('Informações do Naive Bayes')
-  print(f'Quantidade de propriedades: {naive_bayes.n_features_in_}')
-  print(f'Classes: {naive_bayes.classes_}')
-  print(f'Quantidade de registros por classe: {naive_bayes.class_count_}')
-  print(f'Percentual de registros por classe: {naive_bayes.class_prior_}')
-  print(f'Acurácia do modelo: {(acuracia*100):,.2f}% ({acuracia})')
-  print(classification_report(classificacoes_teste, previsoes))
-  print(tabela)
-  print('===================================================================================')
 
   return acuracia
